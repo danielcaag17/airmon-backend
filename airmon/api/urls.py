12ab19b364ddb48f0889289311287e0ed1c860ae
Gerard Gispert Carreras <gerardgispertcarreras@gmail.com>
2024-03-07 09:42:50 +0100

    Setup: initial project

---
+++ airmon/api/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("endpoint1/", views.Endpoint1View.as_view(), name="endpoint1"),
+    path("endpoint2/", views.Endpoint2View.as_view(), name="endpoint2"),
+    # Define more URL patterns as needed
+]
