bd69eb9fa1e23c70c0089f4455db9cc5d940ab95
Snake <lluisponsvidal@gmail.com>
2024-03-12 15:36:36 +0100

    T-65: Views structure finished
    I've prepared the structure for views in order to have a more clear file distribution in the project. I've also added an example endpoint as an example of how to make it.

--- airmon/api/urls.py
+++ airmon/api/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("endpoint1/", views.Endpoint1View.as_view(), name="endpoint1"),
-    path("endpoint2/", views.Endpoint2View.as_view(), name="endpoint2"),
+    # path("endpoint2/", views.Endpoint2View.as_view(), name="endpoint2"),
     # Define more URL patterns as needed
 ]
