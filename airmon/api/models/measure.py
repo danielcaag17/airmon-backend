d3856bd0f47f3bb2aa25423e5b1d428bb4c05db3
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2024-03-15 18:19:01 +0100

    T-62 Correccio, si s'elimina una estacio tambe s'elimina la measure associada

--- airmon/api/models/measure.py
+++ airmon/api/models/measure.py
@@ -5,7 +5,7 @@ from .station import Station
 
 class Measure (models.Model):
     # Si s'elimina la estacio, per cascada s'elimina les mesures que estiguin relacionades
-    station_code = models.ForeignKey(Station, on_delete=models.CASCADE, null=True)
+    station_code = models.ForeignKey(Station, on_delete=models.CASCADE)
     date = models.DateField()
     hour = models.TimeField()
 
