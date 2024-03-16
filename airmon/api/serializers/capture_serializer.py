f8934b0fb53dace5bbbfdb0041fc54b53c2caa2c
Snake <lluisponsvidal@gmail.com>
2024-03-16 17:44:05 +0100

    T-63 fix de capture serializer + typo d'atribut attemps d'airmon

--- airmon/api/serializers/capture_serializer.py
+++ airmon/api/serializers/capture_serializer.py
@@ -6,5 +6,5 @@ from ..models import Capture
 class CaptureSerializer(serializers.ModelSerializer):
     class Meta:
         model = Capture
-        fields = ['username', 'airmon_name', 'rarity', 'type', 'date', 'attempts']
+        fields = ['username', 'airmon', 'date', 'attempts']
 
