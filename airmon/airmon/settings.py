f375addcebec5a4dec8df28a546dd9de6a1911c0
Gerard Gispert Carreras <gerardgispertcarreras@gmail.com>
2024-03-19 06:40:49 +0100

    T-51: RDS DB configured - SQLite has been removed - The app has been configured to work with the postgres db hosted at aws - Initial migrations has been setup

--- airmon/airmon/settings.py
+++ airmon/airmon/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = "airmon.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'AirmonDB',
+        'USER': 'admindb',
+        'PASSWORD': 'admin123',
+        'HOST': 'airmondbinstance.czsyqsqokuz8.eu-north-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
 
