e67ef57e685c11ad0ddaac2f0f8f0f547aee7fa4
Snake <68821487+SnakeAmazing@users.noreply.github.com>
2024-03-20 22:44:25 +0100

    Afegit l'url a la carpeta de media per poder servir imatges amb l'api

--- airmon/airmon/settings.py
+++ airmon/airmon/settings.py
@@ -125,6 +125,9 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = "/home/ubuntu/airmon-backend/airmon/static/"
 
+MEDIA_URL = "/media/"
+MEDIA_ROOT = "/home/ubuntu/airmon-backend/airmon/media/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
