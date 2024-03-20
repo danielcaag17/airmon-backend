79414d3bfe9486a2dc1b85dc165e9ff9ba1774bf
Snake <lluisponsvidal@gmail.com>
2024-03-20 23:39:49 +0100

    fix del serializer d'airmon

--- airmon/api/serializers/airmon_serializer.py
+++ airmon/api/serializers/airmon_serializer.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
-from ..models import Airmon
+from ..models import Airmon, RarityType, AirmonType
 
 
 class AirmonSerializer(serializers.ModelSerializer):
+    rarity = serializers.ChoiceField(choices=[(tag.value, tag.value) for tag in RarityType])
+    type = serializers.ChoiceField(choices=[(tag.value, tag.value) for tag in AirmonType])
+
     class Meta:
         model = Airmon
         fields = ['id', 'name', 'description', 'rarity', 'type', 'image']
-
