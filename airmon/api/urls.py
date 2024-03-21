7d92dcdd9b6a754034bfc219aa523902af8b46df
danielcaag <daniel.canizares@estudiantat.upc.edu>
2024-03-21 10:00:48 +0100

    T-57 Eliminat una URL innecessaria

--- airmon/api/urls.py
+++ airmon/api/urls.py
@@ -6,7 +6,6 @@ router = DefaultRouter()
 router.register(r'map', views.MapViewSet, basename='map')
 
 urlpatterns = router.urls + [
-    path('map/', views.MapViewSet.as_view({'get': 'list'}), name='map'),
     # path('', include(router.urls)),
     # path("endpoint1/", views.Endpoint1View.as_view(), name="endpoint1"),
     # path("endpoint2/", views.Endpoint2View.as_view(), name="endpoint2"),
