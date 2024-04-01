7f897577b8aecd88f1550d5603ea1783e883cc9b
Snake <lluisponsvidal@gmail.com>
2024-04-01 16:57:44 +0200

    api protegida baix autoritzacio via token

---
+++ airmon/api/views/auth_view.py
@@ -0,0 +1,47 @@
+from django.contrib.auth.models import User
+from django.shortcuts import get_object_or_404
+from rest_framework.authtoken.models import Token
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from ..serializers.user_serializer import UserSerializer
+
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import SessionAuthentication, TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+
+@api_view(['POST'])
+def login(request):
+    username = request.data['username']
+    password = request.data['password']
+
+    if username is None or password is None:
+        return Response({'error': 'Please provide both username and password'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    user = get_object_or_404(User, username=username)
+    if not user.check_password(password):
+        return Response({'detail': 'Not found.'},
+                        status=status.HTTP_400_BAD_REQUEST)
+
+    token, created = Token.objects.get_or_create(user=user)
+    return Response({'token': token.key}, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+def register(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        user = serializer.save()
+        token = Token.objects.create(user=user)
+        return Response({'token': token.key, 'user': serializer.data}, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication, TokenAuthentication])
+@permission_classes([IsAuthenticated])
+def test_token(request):
+    return Response({'detail': 'Token is valid for {}'.format(request.user.username)}, status=status.HTTP_200_OK)
