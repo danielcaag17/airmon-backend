99bc2ed6e62313ea2d0d4995396432e990b233dc
danielcaag <daniel.canizares@estudiantat.upc.edu>
2024-04-02 08:57:24 +0200

    fix - mida de name ampliada

--- airmon/api/models/station.py
+++ airmon/api/models/station.py
@@ -5,5 +5,5 @@ from .location import Location
 
 class Station (models.Model):
     code = models.CharField(max_length=32, primary_key=True)
-    name = models.CharField(max_length=32)
+    name = models.CharField(max_length=64)
     location = models.ForeignKey(Location, on_delete=models.SET_NULL, null=True)
