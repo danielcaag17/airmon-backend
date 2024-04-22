714e838635094a6776d09ac1813f63b68b4d72e9
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2024-04-22 17:12:56 +0200

    T-90 Calcul ICQA a partir dels serializers

--- airmon/api/serializers/__init__.py
+++ airmon/api/serializers/__init__.py
@@ -2,3 +2,4 @@ from .capture_serializer import CaptureSerializer
 from .station_serializer import StationSerializer
 from .airmon_serializer import AirmonSerializer
 from .user_serializer import UserSerializer
+from .measure_serializer import MeasureSerializer
