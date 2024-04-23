ce35bbc3dc311f5fc76d98859c80d6bdc851358d
danielcaag <daniel.canizares@estudiantat.upc.edu>
2024-04-23 08:59:16 +0200

    T-106 Check per a no tenir una amistat amb el mateix user

--- airmon/api/models/friendship.py
+++ airmon/api/models/friendship.py
@@ -1,12 +1,23 @@
+from django.core.exceptions import ValidationError
 from django.db import models
+from django.db.models import CheckConstraint, Q, F
 from rest_framework.authtoken.admin import User
 
 
 class FriendShip(models.Model):
-    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user1')
-    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user2')
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user1_friendship')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user2_friendship')
     date = models.DateTimeField()
 
+    def create(self, **kwargs):
+        new_friendship = FriendShip(**kwargs)
+        new_friendship.clean()
+        new_friendship.save()
+        return new_friendship
+
+    def clean(self):
+        if self.user1 == self.user2:
+            raise ValidationError('Users cannot be the same')
+
     class Meta:
-        # Falta comprovar quan es crea un usuari que no es ell mateix
         unique_together = [['user1', 'user2'], ['user2', 'user1']]
