eb73b8be1e125bbce177a7d2efa326c820abbd75
danielcaag <daniel.canizares@estudiantat.upc.edu>
2024-04-25 08:26:27 +0200

    Refix de station serializer

--- airmon/api/serializers/station_serializer.py
+++ airmon/api/serializers/station_serializer.py
@@ -25,7 +25,7 @@ class StationSerializer(serializers.ModelSerializer):
     def get_measure(self, obj):
         measures = Measure.objects.filter(station_code=obj.code)
         serializer = MeasureSerializer(measures, many=True)
-        if bool(serializer.data[0]):
+        if bool(serializer.data):
             return serializer.data[0]
         else:
             return None
