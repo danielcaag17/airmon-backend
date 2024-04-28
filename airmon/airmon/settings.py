457663d7462298ab8e50c262b1e6988f1ea002b3
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2024-04-28 14:40:02 +0200

    T-126 Tests del model de Airmon amb imatge

--- airmon/airmon/settings.py
+++ airmon/airmon/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
-
+import sys
 from pathlib import Path
 from celery.schedules import crontab
 from dotenv import load_dotenv
@@ -102,7 +102,11 @@ else:
         }
     }
 
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+if 'test' in sys.argv:
+    DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+else:
+    DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 AWS_ACCESS_KEY_ID = os.environ.get("AWS_ACCESS_KEY_ID")
 AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
--- airmon/api/tests/test_airmon.py
+++ airmon/api/tests/test_airmon.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.core.files import File
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 from ..models import Airmon
 
@@ -10,6 +12,12 @@ class AirmonModelTest(TestCase):
             description="Description simple",
         )
 
+        image_path = 'airmon/api/tests/media-test/airmon-test.png'
+        with open(image_path, 'rb') as f:
+            image_file = File(f)
+            self.uploaded_image = SimpleUploadedFile(image_file.name, image_file.read())
+
+
     # Crear un Airmon simple
     def test_airmon_creation(self):
         airmon = Airmon.objects.get(name="Test airmon simple")
@@ -17,36 +25,33 @@ class AirmonModelTest(TestCase):
         self.assertEqual(airmon.description, "Description simple")
         self.assertEqual(airmon.rarity, "")
         self.assertEqual(airmon.type, "")
-        # Canviar model si es necessari
-        # self.assertEqual(airmon.image, None)
+        self.assertEqual(airmon.image.name, "")
 
-    # Crear airmon amb tots els atributs TODO
+    # Crear airmon amb tots els atributs
     def test_airmon_creation2(self):
         airmon = Airmon.objects.create(
             name="Airmon creation2",
             description="Description",
             rarity="Llegendari",
             type="Lorem",
-            image=None
+            image=self.uploaded_image
         )
         self.assertEqual(airmon.name, "Airmon creation2")
         self.assertEqual(airmon.description, "Description")
         self.assertEqual(airmon.rarity, "Llegendari")
         self.assertEqual(airmon.type, "Lorem")
-        self.assertEqual(airmon.image, None)
-
-
+        self.assertNotEquals(airmon.image.name, "")
 
     # Eliminar Airmon simple
     def test_aimon_destroy(self):
-        airmons_before = Airmon.objects.count() # Nombre de Airmons
+        airmons_before = Airmon.objects.count()  # Nombre de Airmons que hi ha
         airmon = Airmon.objects.get(name="Test airmon simple")
         airmon.delete()
         airmons_after = Airmon.objects.count()
         self.assertEqual(airmons_after, airmons_before - 1)
 
     # Modificar PK Airmon simple
-    def test_airmon_update_attributes(self):
+    def test_airmon_update_PK(self):
         airmon = Airmon.objects.get(name="Test airmon simple")
         airmon.name = "Airmon renamed"
         airmon.save()
@@ -56,6 +61,9 @@ class AirmonModelTest(TestCase):
     def test_airmon_update_attributes(self):
         airmon = Airmon.objects.get(name="Test airmon simple")
         airmon.description = "Description updated"
+        airmon.raity = "Llegendari"
+        airmon.type = "Lorem"
+        airmon.image = self.uploaded_image
         airmon.save()
         self.assertEqual(airmon.description, "Description updated")
 
