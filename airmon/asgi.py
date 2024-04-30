18e1fc1f42d0094e792bac05b3e57b8d4c8a3cf7
lluis <lluisponsvidal@gmail.com>
2024-04-30 09:25:33 +0200

    ISSUE-137 fix issue with project structure

--- airmon/asgi.py
+++ airmon/asgi.py
@@ -13,7 +13,7 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-from airmon.api.routing import websocket_urlpatterns
+from api.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'airmon.settings')
 
