6d7bb9cc33db0d69b898bd34b0f8c6441b0cd137
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2024-05-01 21:03:11 +0200

    T-89 Correccio dels errors

--- api/utils/air_api_util.py
+++ api/utils/air_api_util.py
@@ -116,120 +116,118 @@ def _get_air_measurement(data):
     return measure
 
 
-def calcular_icqa(pollutants):
+def calcular_icqa(pollutant):
     val_max = 0
     val_color = 0
     nom_pollutant = ""
-    for pollutant in pollutants:
-        if pollutants.exists():
-            name = pollutant.pollutant_name.name
-            quantity = pollutant.quantity
-            if name == "NO2":
-                if quantity <= 40:
-                    val_color = 1
-                elif quantity <= 90:
-                    val_color = 2
-                elif quantity <= 120:
-                    val_color = 3
-                elif quantity <= 230:
-                    val_color = 4
-                elif quantity <= 340:
-                    val_color = 5
-                else:
-                    val_color = 6
-            elif name == "PM10":
-                if quantity <= 20:
-                    val_color = 1
-                elif quantity <= 40:
-                    val_color = 2
-                elif quantity <= 50:
-                    val_color = 3
-                elif quantity <= 100:
-                    val_color = 4
-                elif quantity <= 150:
-                    val_color = 5
-                else:
-                    val_color = 6
-            elif name == "PM2.5":
-                if quantity <= 10:
-                    val_color = 1
-                elif quantity <= 20:
-                    val_color = 2
-                elif quantity <= 25:
-                    val_color = 3
-                elif quantity <= 50:
-                    val_color = 4
-                elif quantity <= 75:
-                    val_color = 5
-                else:
-                    val_color = 6
-            elif name == "O3":
-                if quantity <= 50:
-                    val_color = 1
-                elif quantity <= 100:
-                    val_color = 2
-                elif quantity <= 130:
-                    val_color = 3
-                elif quantity <= 240:
-                    val_color = 4
-                elif quantity <= 380:
-                    val_color = 5
-                else:
-                    val_color = 6
-            elif name == "SO2":
-                if quantity <= 100:
-                    val_color = 1
-                elif quantity <= 200:
-                    val_color = 2
-                elif quantity <= 350:
-                    val_color = 3
-                elif quantity <= 500:
-                    val_color = 4
-                elif quantity <= 750:
-                    val_color = 5
-                else:
-                    val_color = 6
-            elif name == "CO":
-                if quantity <= 2:
-                    val_color = 1
-                elif quantity <= 5:
-                    val_color = 2
-                elif quantity <= 10:
-                    val_color = 3
-                elif quantity <= 20:
-                    val_color = 4
-                elif quantity <= 50:
-                    val_color = 5
-                else:
-                    val_color = 6
-            elif name == "C6H6":
-                if quantity <= 5:
-                    val_color = 1
-                elif quantity <= 10:
-                    val_color = 2
-                elif quantity <= 20:
-                    val_color = 3
-                elif quantity <= 50:
-                    val_color = 4
-                elif quantity <= 100:
-                    val_color = 5
-                else:
-                    val_color = 6
-            elif name == "H2S":
-                if quantity <= 25:
-                    val_color = 1
-                elif quantity <= 50:
-                    val_color = 2
-                elif quantity <= 100:
-                    val_color = 3
-                elif quantity <= 200:
-                    val_color = 4
-                elif quantity <= 500:
-                    val_color = 5
-                else:
-                    val_color = 6
+    name = pollutant.pollutant_name.name
+    quantity = pollutant.quantity
+    if name == "NO2":
+        if quantity <= 40:
+            val_color = 1
+        elif quantity <= 90:
+            val_color = 2
+        elif quantity <= 120:
+            val_color = 3
+        elif quantity <= 230:
+            val_color = 4
+        elif quantity <= 340:
+            val_color = 5
+        else:
+            val_color = 6
+    elif name == "PM10":
+        if quantity <= 20:
+            val_color = 1
+        elif quantity <= 40:
+            val_color = 2
+        elif quantity <= 50:
+            val_color = 3
+        elif quantity <= 100:
+            val_color = 4
+        elif quantity <= 150:
+            val_color = 5
+        else:
+            val_color = 6
+    elif name == "PM2.5":
+        if quantity <= 10:
+            val_color = 1
+        elif quantity <= 20:
+            val_color = 2
+        elif quantity <= 25:
+            val_color = 3
+        elif quantity <= 50:
+            val_color = 4
+        elif quantity <= 75:
+            val_color = 5
+        else:
+            val_color = 6
+    elif name == "O3":
+        if quantity <= 50:
+            val_color = 1
+        elif quantity <= 100:
+            val_color = 2
+        elif quantity <= 130:
+            val_color = 3
+        elif quantity <= 240:
+            val_color = 4
+        elif quantity <= 380:
+            val_color = 5
+        else:
+            val_color = 6
+    elif name == "SO2":
+        if quantity <= 100:
+            val_color = 1
+        elif quantity <= 200:
+            val_color = 2
+        elif quantity <= 350:
+            val_color = 3
+        elif quantity <= 500:
+            val_color = 4
+        elif quantity <= 750:
+            val_color = 5
+        else:
+            val_color = 6
+    elif name == "CO":
+        if quantity <= 2:
+            val_color = 1
+        elif quantity <= 5:
+            val_color = 2
+        elif quantity <= 10:
+            val_color = 3
+        elif quantity <= 20:
+            val_color = 4
+        elif quantity <= 50:
+            val_color = 5
+        else:
+            val_color = 6
+    elif name == "C6H6":
+        if quantity <= 5:
+            val_color = 1
+        elif quantity <= 10:
+            val_color = 2
+        elif quantity <= 20:
+            val_color = 3
+        elif quantity <= 50:
+            val_color = 4
+        elif quantity <= 100:
+            val_color = 5
+        else:
+            val_color = 6
+    elif name == "H2S":
+        if quantity <= 25:
+            val_color = 1
+        elif quantity <= 50:
+            val_color = 2
+        elif quantity <= 100:
+            val_color = 3
+        elif quantity <= 200:
+            val_color = 4
+        elif quantity <= 500:
+            val_color = 5
+        else:
+            val_color = 6
 
-            if val_max < val_color:
-                val_max = val_color
-                nom_pollutant = name
+    if val_max < val_color:
+        val_max = val_color
+        nom_pollutant = name
     return val_max, nom_pollutant
