6625de28242e1a09705e2c9e3c3d6bda57869da8
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2024-05-02 20:00:46 +0200

    T-126 Test del Model Item acabat amb migracio

--- api/tests/test_item.py
+++ api/tests/test_item.py
@@ -1,3 +1,4 @@
+from decimal import Decimal
 from django.db import IntegrityError
 from django.test import TestCase
 
@@ -8,16 +9,34 @@ class ItemModelTest(TestCase):
     def setUp(self):
         self.item = Item.objects.create(
             name="Item test",
-            rarity="Legendary",
+            rarity="Llegendari",
             price=29.99,
             description="Description"
         )
 
     def test_item_creation(self):
         self.assertEqual(self.item.name, "Item test")
-        self.assertEqual(self.item.rarity, "Legendary")
+        self.assertEqual(self.item.rarity, "Llegendari")
         self.assertEqual(self.item.price, 29.99)
         self.assertEqual(self.item.description, "Description")
+
+    # Crear un item amb més de dos decimals
+    def test_item_creation2(self):
+        item = Item.objects.create(
+            name="Item",
+            rarity="Epic",
+            price=10.529424782,
+            description="Description"
+        )
+        self.assertEqual(item.price, 10.53)
+
+        item2 = Item.objects.create(
+            name="Item2",
+            rarity="Epic",
+            price=10.52111,
+            description="Description"
+        )
+        self.assertEqual(item2.price, 10.52)
 
     def test_item_destroy(self):
         items_before = Item.objects.count()  # Nombre de Items que hi ha
@@ -36,6 +55,16 @@ class ItemModelTest(TestCase):
         self.assertEqual(self.item.price, 19.99)
         self.assertEqual(self.item.description, "Description updated")
 
+    # Modificar el preu d'un item amb més de dos decimals
+    def test_item_update2(self):
+        self.item.price = 32.2498324
+        self.item.save()
+        self.assertEqual(self.item.price, 32.25)
+
+        self.item.price = 32.24111
+        self.item.save()
+        self.assertEqual(self.item.price, 32.24)
+
     def test_item_invalid1(self):
         try:
             Item.objects.create(
@@ -44,8 +73,22 @@ class ItemModelTest(TestCase):
                 price=10.50,
                 description="Description invalid"
             )
-            self.fail("It should raise an exception")
+            self.fail("It should raise an exception, item invalid1")
         except IntegrityError as e:
             self.assertIsInstance(e, IntegrityError)
             self.assertIn("UNIQUE constraint failed: api_item.name", str(e))
+
+    # Crear item amb una raresa que no existeix
+    def test_item_invalid2(self):
+        try:
+            Item.objects.create(
+                name="Item invalid",
+                rarity="Rarity que no existeix",
+                price=10.50,
+                description="Description invalid"
+            )
+            self.fail("It should raise an exception, item invalid2")
+        except ValueError as e:
+            self.assertIsInstance(e, ValueError)
+            self.assertIn("Invalid rarity value.", str(e))
 
