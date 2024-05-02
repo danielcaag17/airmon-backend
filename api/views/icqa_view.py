0b1e06d3b2fda3cfd83ec653c3b4ecadd68062db
Gerard Gispert Carreras <gerardgispertcarreras@gmail.com>
2024-05-02 22:25:20 +0200

    T-58: Servei extern sense autenticació

--- api/views/icqa_view.py
+++ api/views/icqa_view.py
@@ -1,18 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from rest_framework.decorators import authentication_classes, permission_classes
-from rest_framework.authentication import TokenAuthentication
-from rest_framework.permissions import IsAuthenticated
-
 from api.models.measure import Measure
+from api.models.location import LocationGeohash
 from api.utils.icqa_by_area import ICQA_VALORATION, get_stations_in_area
 
-from ..models import LocationGeohash
 
-
-# @authentication_classes([TokenAuthentication])
-# @permission_classes([IsAuthenticated])
 class ICQAView(APIView):
     """
     Endpoint: /api/icqa/
