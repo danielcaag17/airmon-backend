c308efd70194339058f639d5c611a9c2b8fa293c
lluis <lluisponsvidal@gmail.com>
2024-05-03 13:04:59 +0200

    removed unnecessary player id from PlayerImageViewSet

--- api/serializers/player_serializer.py
+++ api/serializers/player_serializer.py
@@ -14,5 +14,5 @@ class PlayerSerializer(serializers.ModelSerializer):
 class PlayerImagesSerializer(serializers.ModelSerializer):
     class Meta:
         model = PlayerImages
-        fields = ['user', 'image', 'date']
+        fields = ['image', 'date']
 
