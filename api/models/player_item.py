b0281cc2eef03ffc661d60ac5c327c8a950651df
lluis <lluisponsvidal@gmail.com>
2024-05-14 09:43:33 +0200

    redefined item models

--- api/models/player_item.py
+++ api/models/player_item.py
@@ -4,10 +4,10 @@ from .item import Item
 from django.contrib.auth.models import User
 
 
-class PlayerItem (models.Model):
+class PlayerItem(models.Model):
     # En cas que s'elimini l'Item o l'Usuari s'elimen tots els ItemPlayer que es relacionen
     item_name = models.ForeignKey(Item, on_delete=models.CASCADE)
-    username = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField()
 
     class Meta:
