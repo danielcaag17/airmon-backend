899d2c39a8251efd00cb78481aae83d7dd383452
Daniel Cañizares <147058221+danielcaag17@users.noreply.github.com>
2024-05-16 14:50:40 +0200

    posar autenticacio a la vista

--- api/views/player_trophy_view.py
+++ api/views/player_trophy_view.py
@@ -4,7 +4,13 @@ from rest_framework.response import Response
 from ..models import PlayerTrophy, Trophy
 from ..serializers import TrophySerializer
 
+from rest_framework.decorators import authentication_classes, permission_classes
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 
+
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 class PlayerTrophyViewSet(viewsets.ViewSet):
     def list(self, request, username=None):
         all_trophies = Trophy.objects.all()
