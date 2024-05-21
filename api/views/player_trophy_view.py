42f414f3ae78e370e112e0a40f8148418466ff8b
Daniel Cañizares <daniel.canizares@estudiantat.upc.edu>
2024-05-21 12:33:56 +0200

    Modificacions per fer el merge

--- api/views/player_trophy_view.py
+++ api/views/player_trophy_view.py
@@ -9,8 +9,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 
-@authentication_classes([TokenAuthentication])
-@permission_classes([IsAuthenticated])
+# @authentication_classes([TokenAuthentication])
+# @permission_classes([IsAuthenticated])
 class PlayerTrophyViewSet(viewsets.ViewSet):
     def list(self, request, username=None):
         all_trophies = Trophy.objects.all()
@@ -18,10 +18,8 @@ class PlayerTrophyViewSet(viewsets.ViewSet):
         for trophy in serializer.data:
             # Cas per defecte, user no te el trofeu
             trophy["data"] = None
-            trophies_obtained = PlayerTrophy.objects.filter(trophy=trophy["id"])
-            for trophy_obtained in trophies_obtained:
-                if trophy_obtained.username.username == username:
-                    # Si user te el trofeu, es posa la data
-                    trophy["data"] = trophy_obtained.date
+            if PlayerTrophy.objects.filter(trophy_id=trophy["id"], user__username=username).exists():
+                player_trophy = PlayerTrophy.objects.filter(trophy_id=trophy["id"], user__username=username).first()
+                trophy["data"] = player_trophy.date
 
         return Response(serializer.data)
