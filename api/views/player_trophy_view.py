424712849a8f6dbf426c93680793381f7e575cff
Daniel Cañizares <147058221+danielcaag17@users.noreply.github.com>
2024-05-21 13:21:28 +0200

    Update player_trophy_view.py

--- api/views/player_trophy_view.py
+++ api/views/player_trophy_view.py
@@ -9,8 +9,8 @@ from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 
-# @authentication_classes([TokenAuthentication])
-# @permission_classes([IsAuthenticated])
+@authentication_classes([TokenAuthentication])
+@permission_classes([IsAuthenticated])
 class PlayerTrophyViewSet(viewsets.ViewSet):
     def list(self, request, username=None):
         all_trophies = Trophy.objects.all()
