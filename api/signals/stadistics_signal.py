061193c0b5d866d7a988fdd520c2a72cda680e42
danielcaag <daniel.canizares@estudiantat.upc.edu>
2024-05-23 13:09:03 +0200

    T-171-Migració més primera estadística modificada

--- api/signals/stadistics_signal.py
+++ api/signals/stadistics_signal.py
@@ -1,11 +1,12 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from ..models import Capture
+from ..models import Capture, Player
 
 
 @receiver(post_save, sender=Capture)
 def capture_created(sender, instance, created, **kwargs):
     if created:
-        print(f'User {instance.username} has been created.')
+        player = Player.objects.get(username=instance.username)
+        player.n_airmons_capturats += 1
     else:
         print(f'User {instance.username} has been updated.')
