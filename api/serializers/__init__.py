3d75bb7b8f14aa0063dcace8317e8eea0a7ffdfe
danielcaag <daniel.canizares@estudiantat.upc.edu>
2024-05-23 15:35:29 +0200

    T-171-Merge de dev

--- api/serializers/__init__.py
+++ api/serializers/__init__.py
@@ -6,5 +6,5 @@ from .friendship_serializer import FriendshipSerializer
 from .measure_serializer import MeasureSerializer
 from .message_serializer import MessageSerializer
 from .event_serializer import EventSerializer
-from .player_serializer import PlayerSerializer, PlayerPublicSerializer, PlayerImagesSerializer,
+from .player_serializer import PlayerSerializer, PlayerPublicSerializer, PlayerImagesSerializer, PlayerStatisticsSerializer
 from .item_serializer import ItemSerializer, PlayerItemSerializer, PlayerActiveItemSerializer
