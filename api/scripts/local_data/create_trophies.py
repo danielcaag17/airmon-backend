9b6d2320e29e6dc024f1906a75cb7b4a14df3b0a
danielcaag <daniel.canizares@estudiantat.upc.edu>
2024-05-25 17:59:33 +0200

    T-206-Script per crear tots els trofeus

---
+++ api/scripts/local_data/create_trophies.py
@@ -0,0 +1,33 @@
+def create_trophies():
+    import os
+    from django.conf import settings
+
+    from api.scripts.local_data.trophies_data import get_trophies
+    from api.models.trophy import Trophy
+
+    # Trophy.objects.all().delete()
+    new_trophies = []
+    trophies = get_trophies()
+    for trophy in trophies:
+        new_trophy = Trophy(
+            name=trophy["name"],
+            type=trophy["type"].value,
+            description=trophy["description"],
+            requirement=trophy["requirement"],
+            xp=trophy["xp"],
+        )
+
+        new_trophies.append(new_trophy)
+    Trophy.objects.bulk_create(
+        new_trophies,
+        update_conflicts=True,
+        unique_fields=["name", "type"],
+        update_fields=["name", "type", "description", "requirement", "xp"],
+    )
+
+
+if __name__ == "__main__":
+    from api.scripts.django_setup import setup_django
+
+    setup_django()
+    create_trophies()
