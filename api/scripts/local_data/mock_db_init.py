64284e3cfff5532471c127de9a735ba6e2453116
Gerard Gispert Carreras <gerardgispertcarreras@gmail.com>
2024-05-28 16:24:47 +0200

    T-225: Spawns millorats - Més probabilitat de spawn al voltant de events - Spawn points reduits a la meitat - Events fix

--- api/scripts/local_data/mock_db_init.py
+++ api/scripts/local_data/mock_db_init.py
@@ -2,13 +2,14 @@ def mock_db_init():
     from api.scripts.local_data.load_air_data import load_air_data
     from api.scripts.local_data.create_airmons import create_airmons
     from api.scripts.local_data.fill_spawn_points import populate_barcelona_spawn_points
-    from api.utils.airmons_spawn import reset_spawns, spawn_new_airmons
+    from api.scripts.local_data.spawn_airmons import spawn_airmons
+    from api.scripts.local_data.load_event_data import load_event_data
 
     load_air_data()
+    load_event_data()
     create_airmons()
     populate_barcelona_spawn_points()
-    reset_spawns()
-    spawn_new_airmons()
+    spawn_airmons()
 
 
 if __name__ == "__main__":
