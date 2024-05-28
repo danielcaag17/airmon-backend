4cf714f7bb26d5c62fca57c89b2a8bf56346ebb9
Snake <lluisponsvidal@gmail.com>
2024-05-28 15:51:09 +0200

    fix api externa

--- api/models/event.py
+++ api/models/event.py
@@ -5,6 +5,7 @@ from .location import LocationGeohash
 
 class Event(models.Model):
     codi = models.CharField(primary_key=True, max_length=32)
+    denominacio = models.CharField(max_length=256, default="")
     data_ini = models.DateTimeField()
     data_fi = models.DateTimeField()
     geohash = models.ForeignKey(LocationGeohash, on_delete=models.CASCADE)
