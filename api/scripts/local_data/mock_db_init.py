9a80ff1f9e116e40f4268182dce0f4c349a803b8
lluis <lluisponsvidal@gmail.com>
2024-05-29 11:59:00 +0200

    fix [skip ci]

--- api/scripts/local_data/mock_db_init.py
+++ api/scripts/local_data/mock_db_init.py
@@ -4,12 +4,16 @@ def mock_db_init():
     from api.scripts.local_data.fill_spawn_points import populate_barcelona_spawn_points
     from api.scripts.local_data.spawn_airmons import spawn_airmons
     from api.scripts.local_data.load_event_data import load_event_data
+    from api.scripts.local_data.populate_items import populate_items
+    from api.scripts.local_data.create_trophies import create_trophies
 
     load_air_data()
     load_event_data()
     create_airmons()
     populate_barcelona_spawn_points()
     spawn_airmons()
+    populate_items()
+    create_trophies()
 
 
 if __name__ == "__main__":
