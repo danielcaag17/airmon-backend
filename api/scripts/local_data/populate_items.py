424be5f8fe9b03701daa8a5a139d7f22354e7680
lluis <lluisponsvidal@gmail.com>
2024-05-29 11:53:44 +0200

    add info to script

--- api/scripts/local_data/populate_items.py
+++ api/scripts/local_data/populate_items.py
@@ -4,35 +4,37 @@ setup_django()
 
 from api.models import Item
 
-Item.objects.create(
-    name='coin_booster',
-    price=100,
-    description='30% more coins earned for 30 minutes',
-    image='items/coin_booster.png',
-    duration='00:30:00'
-)
 
-Item.objects.create(
-    name='exp_booster',
-    price=100,
-    description='30% more experience earned for 2 hours',
-    image='items/exp_booster.png',
-    duration='02:00:00'
-)
+def populate_items():
+    Item.objects.create(
+        name='coin_booster',
+        price=100,
+        description='30% more coins earned for 30 minutes',
+        image='items/coin_booster.png',
+        duration='00:30:00'
+    )
 
-Item.objects.create(
-    name='extra_roulette_spin',
-    price=100,
-    description='1 extra roulette spin',
-    image='items/extra_roulette_spin.png',
-    duration='00:00:00'
-)
+    Item.objects.create(
+        name='exp_booster',
+        price=100,
+        description='30% more experience earned for 2 hours',
+        image='items/exp_booster.png',
+        duration='02:00:00'
+    )
 
-Item.objects.create(
-    name='airbox',
-    price=1000,
-    description='Open this box to get a random airmon',
-    image='items/airbox.png',
-    duration='00:00:00'
-)
+    Item.objects.create(
+        name='extra_roulette_spin',
+        price=450,
+        description='1 extra roulette spin',
+        image='items/extra_roulette_spin.png',
+        duration='00:00:00'
+    )
+
+    Item.objects.create(
+        name='airbox',
+        price=1000,
+        description='Open this box to get a random airmon',
+        image='items/airbox.png',
+        duration='00:00:00'
+    )
 
